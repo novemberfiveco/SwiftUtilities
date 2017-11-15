859fda696cdefb181e080ac36c9404b642c18cc0
Jens Reynders <jens.reynders@appstrakt.com>
2017-11-15 16:58:15 +0100

    Setup demo project

---
+++ Demo/SwiftUtilities-Demo/General/AppDelegate+Demo.swift
@@ -0,0 +1,16 @@
+//
+//  AppDelegate+Demo.swift
+//  SwiftUtilities-Demo
+//
+//  Created by Jens Reynders on 15/11/2017.
+//  Copyright © 2017 Appstrakt. All rights reserved.
+//
+
+import Foundation
+import SwiftUtilities
+
+extension AppDelegate {
+    func testUtilities() {
+        _ = "check".localized()
+    }
+}
